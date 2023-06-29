e879744ecbbb7ed737af8ca046af8663fb4b1287
jiuseu <109057859+jiuseu@users.noreply.github.com>
2023-06-29 23:31:08 +0900

    Update 단어 공부.java

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,45 +1,38 @@
 import java.io.*;
 import java.util.*;
- 
+import java.util.stream.Collectors;
+
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int [] arr = new int[26];
-		String a = br.readLine();
-		
-		for(int i=0;i<a.length();i++) {
-			
-			if(65<=a.charAt(i) && a.charAt(i) <=90) {
-				arr[a.charAt(i) -65]++;
-			}
-			
-			else {
-				arr[a.charAt(i) -97]++;
-			}
-			
-		}
-		
-		int max = -1;
-		char ch = '?';
-		
-		for(int i =0;i<26;i++) {
-			if(arr[i]>max) {
-				max =arr[i];
-				ch = (char) (i+65);
-			}
-			else if(arr[i]==max) {
-				ch='?';
-			}
-			
-		}
-				
-		
-		
-		System.out.print(ch); 
-		
-		
-	}
- 
+
+    public static void main(String args[])throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        //StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int [] arr = new int[26];
+        String s = br.readLine().toLowerCase();
+
+        for(int i =0;i<s.length();i++){
+            arr[s.charAt(i)-97]++;
+        }
+
+        int max = -1;
+        char ch = '?';
+
+        for(int i=0;i<26;i++){
+            if(arr[i]>max){
+                max=arr[i];
+                ch = (char) (i+65);
+            }
+            else if(arr[i]==max){
+                ch='?';
+            }
+        }
+        
+        bw.write(String.valueOf(ch));
+        bw.flush();
+        bw.close();
+    }
+
 }

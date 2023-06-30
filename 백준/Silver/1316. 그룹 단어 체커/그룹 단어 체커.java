9a1d63e1d63a9f044d2675239c5116a2a8482319
jiuseu <109057859+jiuseu@users.noreply.github.com>
2023-06-30 22:19:18 +0900

    Update 그룹 단어 체커.java

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -1,55 +1,42 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
- 
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
- 
-	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-	public static void main(String[] args) throws IOException{
- 
-		int count = 0;
-		int N = Integer.parseInt(br.readLine());
- 
-		for (int i = 0; i < N; i++) {
-			if (check() == true) {
-				count++;
-			}
-		}
-		System.out.println(count);
-	}
- 
-	public static boolean check() throws IOException {
-		boolean[] check = new boolean[26];
-		int prev = 0;
-		String str = br.readLine();
-		
-		for(int i = 0; i < str.length(); i++) {
-			int now = str.charAt(i);	// i 번째 문자 저장 (현재 문자)
-			
-			
-			// 앞선 문자와 i 번째 문자가 같지 않다면?
-			if (prev != now) {		
-				
-				// 해당 문자가 처음 나오는 경우 (false 인 경우)
-				if ( check[now - 'a'] == false ) {
-					check[now - 'a'] = true;		// true 로 바꿔준다
-					prev = now;					// 다음 턴을 위해 prev 도 바꿔준다 
-				}
-	 
-				// 해당 문자가 이미 나온 적이 있는 경우 (그룹단어가 아니게 됨) 
-				else {
-					return false;	//함수 종료
-				}
-			}
-	        
-	        
-			// 앞선 문자와 i 번째 문자가 같다면? (연속된 문자)
-			// else 문은 없어도 됨
-			else {
-				continue;
-			}
-		}    
-		return true;
-	}
+
+    public static void main(String args[])throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        //StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int n = Integer.parseInt(br.readLine());
+        int count = 0;
+        
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            boolean check[] = new boolean[26]; 
+            boolean bol = true; 
+
+            for (int j = 0; j < s.length(); j++) {
+                int a = s.charAt(j)-'a';
+                if(check[a]) { 
+                    if(s.charAt(j) != s.charAt(j-1)) {
+                        bol = false; 
+                        break;
+                    }
+                }else { 
+                    check[a] = true;
+                }
+            }
+            if(bol) {
+                count++;   
+            }
+        }
+
+        bw.write(String.valueOf(count));
+        bw.flush();
+        bw.close();
+    }
+
 }

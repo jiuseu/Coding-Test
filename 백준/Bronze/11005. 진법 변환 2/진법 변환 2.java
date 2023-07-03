1996c0416725fdd7ea5e91e6eb90fe4dd55833e7
jiuseu <109057859+jiuseu@users.noreply.github.com>
2023-07-03 23:34:45 +0900

    [Bronze I] Title: 진법 변환 2, Time: 128 ms, Memory: 14420 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String args[])throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        bw.write(String.valueOf(Integer.toString(a,b).toUpperCase()));
+
+        bw.flush();
+        bw.close();
+    }
+
+}

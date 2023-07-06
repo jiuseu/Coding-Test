051c0f63beb0ae9606f2a856b406a19a0e56a5c7
jiuseu <109057859+jiuseu@users.noreply.github.com>
2023-07-06 22:51:29 +0900

    Update 소수.java

--- 백준/Bronze/2581. 소수/소수.java
+++ 백준/Bronze/2581. 소수/소수.java
@@ -1,57 +1,59 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
- 
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
- 
-	public static boolean prime[];
-	
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int M = Integer.parseInt(br.readLine());
-		int N = Integer.parseInt(br.readLine());
-		
-		prime = new boolean[N + 1];	// 배열 생성 
-		get_prime();
-		
-		
-		// 소수 합 및 최솟값 
-		int sum = 0;
-		int min = Integer.MAX_VALUE;
-		for(int i = M; i <= N; i++) {
-			if(prime[i] == false) {	// false = 소수 
-				sum += i;
-				if(min == Integer.MAX_VALUE) {	// 첫 소수가 최솟값임  
-					min = i;
-				}
-			}
-		}
-		
-		if(sum == 0) {	// 소수가 없다면 
-			System.out.println(-1);
-		}
-		else {
-			System.out.println(sum);
-			System.out.println(min);
-		}
-		
-	}
- 
-	
-	// 에라토스테네스 체 알고리즘 
-	public static void get_prime() {
-		prime[0] = true;
-		prime[1] = true;
-		
-		for(int i = 2; i <= Math.sqrt(prime.length); i++) {
-			if(prime[i]) continue;	// 이미 체크된 배열일 경우 skip
-			for(int j = i * i; j < prime.length; j += i) {
-				prime[j] = true;
-			}
-		}
-		
-	}
+
+    public static boolean prime[];
+
+    public static void main(String args[])throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        //StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+
+        prime = new boolean[b + 1];
+        get_prime();
+
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+
+        for(int i= a; i <=b; i++){
+            if(prime[i] == false){
+                sum += i;
+                if(min == Integer.MAX_VALUE){
+                    min = i;
+                }
+            }
+        }
+
+        if(sum == 0){
+            bw.write(String.valueOf(-1)+"\n");
+        }
+        else{
+            bw.write(String.valueOf(sum)+"\n");
+            bw.write(String.valueOf(min)+"\n");
+        }
+
+        bw.flush();
+        bw.close();
+    }
+
+    public static void get_prime(){
+        prime[0] = true;
+        prime[1] = true;
+
+        for(int i=2;i <= Math.sqrt(prime.length);i++){
+            if(prime[i])continue;
+
+            for(int j=i*i; j < prime.length; j+=i){
+                prime[j] = true;
+            }
+        }
+    }
+
 }
- 

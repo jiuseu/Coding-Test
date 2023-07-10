029088a4aa405cc1e6064bffe0ea71df44a615fd
jiuseu <109057859+jiuseu@users.noreply.github.com>
2023-07-10 23:39:12 +0900

    [Bronze II] Title: 알고리즘 수업 - 알고리즘의 수행 시간 6, Time: 132 ms, Memory: 14316 KB -BaekjoonHub

---
+++ 백준/Bronze/24267. 알고리즘 수업 － 알고리즘의 수행 시간 6/알고리즘 수업 － 알고리즘의 수행 시간 6.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String args[])throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        //StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        Long a = Long.parseLong(br.readLine());
+
+        bw.write(String.valueOf((a*(a-1)*(a-2))/6)+"\n");
+        bw.write(String.valueOf(3)+"\n");
+
+        bw.flush();
+        bw.close();
+
+        }
+
+    }
+
+

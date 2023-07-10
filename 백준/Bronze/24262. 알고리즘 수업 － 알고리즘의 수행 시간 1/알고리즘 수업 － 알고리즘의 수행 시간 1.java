83661fbe5d9d594422f90a495e025f1814b277c9
jiuseu <109057859+jiuseu@users.noreply.github.com>
2023-07-10 23:03:44 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 124 ms, Memory: 14320 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String args[])throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        //StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+
+        int a = Integer.parseInt(br.readLine());
+
+
+
+        bw.write(String.valueOf(1)+"\n");
+        bw.write(String.valueOf(0)+"\n");
+
+        bw.flush();
+        bw.close();
+
+        }
+
+    }
+
+

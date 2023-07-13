a9b4747b6b471c9a1ff8b61b68e60b8a72721b88
jiuseu <109057859+jiuseu@users.noreply.github.com>
2023-07-13 23:40:58 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2668 ms, Memory: 345432 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -0,0 +1,34 @@
+import java.util.*;
+import java.util.stream.Stream;
+import java.io.*;
+import java.text.*;
+import java.time.*;
+import java.math.*;
+
+public class Main{
+    public static void main(String[] args)throws IOException{
+    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    	//StringTokenizer st = new StringTokenizer(br.readLine());
+        //Scanner sc =new Scanner(System.in);
+    	//StringBuilder sb=new StringBuilder();
+    	int a = Integer.parseInt(br.readLine());
+    	
+    	int [] arr = new int [a];
+    	
+       for(int i=0;i<a;i++) {
+    	   arr[i] = Integer.parseInt(br.readLine());
+       }
+       
+       Arrays.sort(arr);
+       
+       for(int i=0;i<a;i++) {
+    	   bw.write(String.valueOf(arr[i])+"\n");
+       }
+       
+    	
+       bw.flush();
+       bw.close();
+       br.close();
+    }
+}

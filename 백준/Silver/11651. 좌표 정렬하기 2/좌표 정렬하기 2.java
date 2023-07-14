cdec928b7041989ff39875cf6fb2d1ba7722330b
jiuseu <109057859+jiuseu@users.noreply.github.com>
2023-07-14 22:12:15 +0900

    좌표 정렬하기 2.java

--- 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -1,44 +1,44 @@
-import java.util.*;
-import java.util.stream.Stream;
 import java.io.*;
-import java.text.*;
-import java.time.*;
-import java.math.*;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.stream.Collectors;
 
-public class Main{
-    public static void main(String[] args)throws IOException{
-    	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    	//StringTokenizer st = new StringTokenizer(br.readLine());
-        //Scanner sc =new Scanner(System.in);
-    	//StringBuilder sb=new StringBuilder();
+public class Main {
+
+    public static void main(String args[])throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        //StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
        int a = Integer.parseInt(br.readLine());
-       
+
        int [][] arr = new int[a][2];
-       
-       for(int i=0;i<a;i++) {
-    	   StringTokenizer st = new StringTokenizer(br.readLine());
-    	   arr[i][0]=Integer.parseInt(st.nextToken());
-    	   arr[i][1]=Integer.parseInt(st.nextToken());
+
+       for(int i=0; i<a; i++){
+           StringTokenizer st = new StringTokenizer(br.readLine()," ");
+           arr[i][0] = Integer.parseInt(st.nextToken());
+           arr[i][1] = Integer.parseInt(st.nextToken());
        }
-       
-       Arrays.sort(arr,new Comparator<int []>() {
-    	   public int compare(int a1[], int a2[]) {
-    		   if(a1[1]==a2[1]) {
-    			   return a1[0]-a2[0];
-    		   }
-    		   else {
-    			   return a1[1]-a2[1];
-    		   }
-    	   }
+
+       Arrays.sort(arr, new Comparator<int[]>() {
+           public int compare(int[] a1, int[] a2){
+               if(a1[1] == a2[1]){
+                   return a1[0] - a2[0];
+               }
+               else{
+                   return a1[1] - a2[1];
+               }
+           }
        });
-       
-       for(int i=0;i<a;i++) {
-    	   bw.write(arr[i][0]+" "+arr[i][1]+"\n");
+
+       for(int i=0;i<a;i++){
+           bw.write(arr[i][0]+" "+arr[i][1]+"\n");
        }
-      bw.flush();
-      bw.close();
-      br.close();
+       bw.flush();
+        bw.close();
+
+        }
     }
 
-}
+

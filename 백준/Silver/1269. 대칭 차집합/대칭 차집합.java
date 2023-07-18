d874d8fe1c89c3ca6898c7640358d3e8a9f7c1cb
jiuseu <109057859+jiuseu@users.noreply.github.com>
2023-07-18 23:32:50 +0900

    대칭 차집합.java

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -1,46 +1,45 @@
-import java.util.*;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import java.io.*;
-import java.lang.reflect.Array;
-import java.text.*;
-import java.time.*;
-import java.math.*;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
-	
-  public static void main(String[] args)throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	//Scanner sc = new Scanner(System.in);
-    StringTokenizer st = new StringTokenizer(br.readLine()," ");
-    //StringBuilder sb = new StringBuilder();
-    int a = Integer.parseInt(st.nextToken()); 
-    int b = Integer.parseInt(st.nextToken());
-    
-    HashMap<Integer,Integer>list1 = new HashMap<Integer,Integer>();
-    //HashMap<Integer,Integer>list2 = new HashMap<Integer,Integer>();
-    //ArrayList<Integer>answer = new ArrayList<Integer>();
-    
-    st = new StringTokenizer(br.readLine()," ");
-    for(int i=0;i<a;i++) {
-    	int c = Integer.parseInt(st.nextToken());
-    	list1.put(c, c);
+
+    public static void main(String args[])throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        HashMap<Integer,Integer>list = new HashMap<Integer,Integer>();
+
+        st = new StringTokenizer(br.readLine()," ");
+        for(int i = 0; i < a; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            list.put(num,num);
+        }
+
+        st = new StringTokenizer(br.readLine()," ");
+        for(int i = 0; i < b; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            if(list.get(num)!=null){
+                list.remove(num);
+            }
+            else{
+                list.put(num,num);
+            }
+        }
+
+        bw.write(String.valueOf(list.size()));
+
+        bw.flush();
+        bw.close();
+
+        }
+
     }
-    
-    st = new StringTokenizer(br.readLine()," ");
-    for(int i=0;i<b;i++) {
-    	int c = Integer.parseInt(st.nextToken());
-    	if(list1.get(c)!=null) {
-    		list1.remove(c);
-    	}
-    	else {
-    		list1.put(c,c);
-    	}
-    }
-    
-    bw.write(String.valueOf(list1.size()));
-    bw.flush();
-}
-}
+
+

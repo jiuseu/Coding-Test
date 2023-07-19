f3ba747ffc1cc6d68f2dbfaae6a5e3a9f09ea979
jiuseu <109057859+jiuseu@users.noreply.github.com>
2023-07-19 22:51:22 +0900

    최소공배수.java

--- 백준/Silver/13241. 최소공배수/최소공배수.java
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -1,34 +1,42 @@
+import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import java.io.*;
-import java.text.*;
-import java.time.*;
-import java.math.*;
 
 public class Main {
-  public static void main(String[] args)throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	//Scanner scan = new Scanner(System.in);
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    //StringBuilder sb = new StringBuilder();
 
-    long a = Long.parseLong(st.nextToken());
-    long b = Long.parseLong(st.nextToken());
-    
-    System.out.println(ICM(a,b));
-  }
-public static long GCD(long a,long b) {
-	if(b==0) {
-		return a;
-	}
-	long max = Math.max(a, b);
-	long min = Math.min(a, b);
-	
-	return GCD(min, max%min);
-}
-public static long ICM(long a,long b) {
-	return a*b/GCD(a,b);
-}
- }
+    public static void main(String args[])throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        //StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+            StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+            long b = Long.parseLong(st.nextToken());
+            long c = Long.parseLong(st.nextToken());
+
+            long ans = gcd(b,c);
+
+
+        bw.write(String.valueOf(b*c/ans));
+        bw.flush();
+        bw.close();
+
+        }
+
+        public static long gcd(long a,long b){
+
+        while(b !=0){
+            long num = a%b;
+
+            a = b;
+            b = num;
+        }
+        return a;
+
+        }
+
+    }
+
+

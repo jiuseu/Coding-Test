976cbc286baf9b90a3b1cb5e8375af28fbf2584e
jiuseu <109057859+jiuseu@users.noreply.github.com>
2023-07-26 23:09:08 +0900

    균형잡힌 세상.java

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.java
@@ -1,77 +1,73 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-import java.util.Stack;
- 
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
- 
-		String s;
-		
-		while(true) {
-			
-			s = br.readLine();
-			
-			if(s.equals(".")) {	// 종료 조건문 
-				break;
-			}
-			
-			sb.append(solve(s)).append('\n');
-		}
-		
-		System.out.println(sb);
-		
-		
-	}
-	
-	public static String solve(String s) {
-		
-		Stack<Character> stack = new Stack<>();
-		
-		for(int i = 0; i < s.length(); i++) {
-			
-			char c = s.charAt(i);	// i 번째 문자 
-			
-			// 여는 괄호일 경우 스택에 push 
-			if(c == '(' || c == '[') {
-				stack.push(c);
-			}
-			
-			// 닫는 소괄호 일 경우 
-			else if(c == ')') {
-				
-				// 스택이 비어있거나 pop할 원소가 소괄호랑 매칭이 안되는 경우 
-				if(stack.empty() || stack.peek() != '(') {
-					return "no";
-				}
-				else {
-					stack.pop();
-				}
-			}
-			
-			else if(c == ']') {
-				
-				// 스택이 비어있거나 pop할 원소가 대괄호랑 매칭이 안되는 경우 
-				if(stack.empty() || stack.peek() != '[') {
-					return "no";
-				}
-				else {
-					stack.pop();
-				}
-			}
-			
-			// 그 외의 경우에는 불필요한 문자들이기에 skip한다. 
-		}
-		
-		if(stack.empty()) {
-			return "yes";
-		}
-		else {
-			return "no";
-		}
-	}
- 
+
+    public static void main(String args[])throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        //StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        while(true) {
+            String input = br.readLine();
+            Stack<Character> stack = new Stack<Character>();
+
+            if(input.equals(".")){
+                break;
+            }
+
+            for(int i = 0; i < input.length(); i++) {
+                char ch = input.charAt(i);
+
+                if(ch == '(' || ch == '['){
+                    stack.push(ch);
+                }
+
+                if(ch == ')') {
+                    if(stack.empty()) {
+                        stack.push(ch);
+                        break;
+                    }
+                    if(stack.peek() == '(') {
+                        stack.pop();
+                    }
+                    else {
+                        break;
+                    }
+                }else if(ch == ']') {
+                    if(stack.empty()) {
+                        stack.push(ch);
+                        break;
+                    }
+
+                    if(stack.peek() == '['){
+                        stack.pop();
+                    }
+                    else {
+                        break;
+                    }
+                }
+            }
+
+            if(stack.isEmpty()){
+                sb.append("yes").append("\n");
+            }
+            else {
+                sb.append("no").append("\n");
+            }
+        }
+
+
+        bw.write(String.valueOf(sb));
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
 }
+
+

b7c273369a450215508c0c8f78bb7da0182c7830
jiuseu <109057859+jiuseu@users.noreply.github.com>
2023-07-26 22:27:29 +0900

    스택.java

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,61 +1,63 @@
+import java.io.*;
+import java.math.BigInteger;
 import java.util.*;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import java.io.*;
-import java.lang.reflect.Array;
-import java.text.*;
-import java.time.*;
-import java.math.*;
 
-class Main {
-  
-  public static void main(String[] args)throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	//Scanner sc = new Scanner(System.in);
-    //StringTokenizer st = new StringTokenizer(br.readLine()," ");
-    //StringBuilder sb = new StringBuilder();
-    
-    int a = Integer.parseInt(br.readLine());
-    Stack<Integer>sk = new Stack<Integer>();
-    
-   for(int i=0;i<a;i++) {
-	 String s = br.readLine();
-	 
-	 if(s.contains("push")) {
-		 String [] arr = s.split(" ");
-		 int b= Integer.parseInt(arr[1]);
-		 sk.push(b);
-	 }
-	 else if(s.contains("pop")) {
-		 if(sk.isEmpty()) {
-			 bw.write(String.valueOf(-1)+"\n");	 
-		 }
-		 else {
-			 bw.write(String.valueOf(sk.pop())+"\n");	 
-		 }
-	}
-	else if(s.contains("size")) {
-		bw.write(String.valueOf(sk.size())+"\n");
-	}
-	else if(s.contains("empty")) {
-		if(sk.isEmpty()) {
-			bw.write(String.valueOf(1)+"\n");	
-		}
-		else {
-			bw.write(String.valueOf(0)+"\n");
-		}
-		
-	}
-	else if(s.contains("top")) {
-		if(sk.isEmpty()) {
-		bw.write(String.valueOf(-1)+"\n");
-		}
-		else {
-			bw.write(String.valueOf(sk.peek())+"\n");	
-		}
-	}
-   }
-   bw.flush();
+public class Main {
+
+    public static void main(String args[])throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        //StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int a = Integer.parseInt(br.readLine());
+        Stack<Integer> stack = new Stack<Integer>();
+
+        for(int i=0; i<a; i++){
+            String s = br.readLine();
+            if(s.contains("push")){
+                String[] srr = s.split(" ");
+                int num = Integer.parseInt(srr[1]);
+                stack.push(num);
+            }
+            else if(s.contains("pop")){
+                if(stack.size()!=0){
+                    sb.append(stack.pop()).append("\n");
+                }
+                else{
+                    sb.append(-1).append("\n");
+                }
+            }
+            else if(s.contains("size")){
+                sb.append(stack.size()).append("\n");
+            }
+            else if(s.contains("empty")){
+                if(stack.size()!=0){
+                    sb.append(0).append("\n");
+                }
+                else{
+                    sb.append(1).append("\n");
+                }
+            }
+            else if(s.contains("top")){
+                if(stack.size()!=0){
+                    sb.append(stack.peek()).append("\n");
+                }
+                else{
+                    sb.append(-1).append("\n");
+                }
+            }
+
+        }
+
+        bw.write(String.valueOf(sb));
+        bw.flush();
+        bw.close();
+        br.close();
+
+    }
+
 }
-}
+
+

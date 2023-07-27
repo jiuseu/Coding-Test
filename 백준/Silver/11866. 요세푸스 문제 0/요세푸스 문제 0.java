7ad575ba23f17790079f7205012df50fa8215355
jiuseu <109057859+jiuseu@users.noreply.github.com>
2023-07-27 23:35:01 +0900

    요세푸스 문제 0.java

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -1,55 +1,47 @@
-import java.util.*;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import java.io.*;
-import java.lang.reflect.Array;
-import java.text.*;
-import java.time.*;
-import java.math.*;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.stream.Collectors;
 
-class Main {
-  
-  static StringBuilder sb;
-	
-  public static void main(String[] args)throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-	//Scanner sc = new Scanner(System.in);
-    StringTokenizer st = new StringTokenizer(br.readLine()," ");
-    
-    
-    int a = Integer.parseInt(st.nextToken());
-    int b = Integer.parseInt(st.nextToken());
-    Deque<Integer>q = new LinkedList<Integer>();
-    
-    for(int i=0;i<a;i++) {
-      q.offer(i+1);	
-    }
-    
-    int cnt = 0;
-    
-    String ans ="<";
-    
-    while(!q.isEmpty()) {
-    	cnt++;
-    	
-        if(cnt==b) {
-        int x =q.poll();	
-        ans+=x+", ";
-        cnt = 0;
-    	}
-        else {
-         int x =q.poll();
-         q.offerLast(x);
-         
+public class Main {
+
+    public static void main(String args[])throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int cnt = 1;
+        Deque<Integer> que = new LinkedList<Integer>();
+
+        for(int i=0; i<a; i++){
+            que.offer(i+1);
         }
-        
+
+        String ans = "<";
+
+        while(!que.isEmpty()){
+            if(cnt==b){
+                ans+=que.pollFirst()+", ";
+                cnt=1;
+            }
+            else{
+                que.offer(que.pollFirst());
+                cnt++;
+            }
+        }
+
+        ans = ans.substring(0,ans.length()-2);
+        ans +=">";
+        bw.write(ans);
+        bw.flush();
+        bw.close();
+        br.close();
+
     }
-    ans=ans.substring(0,ans.length()-2);
-    ans+=">";
-    bw.write(ans);
-    bw.flush();
 
 }
-}
+
+

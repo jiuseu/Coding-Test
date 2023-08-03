bec33741ac69d0dad031891c6452bf6a7004e378
jiuseu <109057859+jiuseu@users.noreply.github.com>
2023-08-03 23:47:23 +0900

    피보나치 수 5.java

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -1,27 +1,32 @@
-import java.util.*;
-import java.util.stream.Stream;
 import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+import java.util.stream.Collectors;
 
-public class Main{
-    public static void main(String[] args)throws IOException{
-        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw =new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-      
+public class main {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        //StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        StringBuilder sb= new StringBuilder();
+
         int a = Integer.parseInt(br.readLine());
-        int [] arr = new int[a+1];
-        
-        
-       for(int i=0;i<arr.length;i++) {
-    	   if(i>=2) {
-    		   arr[i]=arr[i-1]+arr[i-2];
-    	   }
-    	   else if(i<2) {
-    		   arr[i]=i;
-    	   }
-    	   
-       }
-       System.out.println(arr[a]);
+
+        bw.write(String.valueOf(fact(a)));
+        bw.flush();
+        bw.close();
+    }
+
+    public static long fact(int n){
+
+        if(n <= 1){
+            return n;
         }
-        
-    }  
+        else{
+            return fact(n-2) + fact(n-1);
+        }
+    }
+}
+
+
